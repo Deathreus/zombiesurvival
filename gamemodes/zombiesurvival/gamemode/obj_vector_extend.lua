4edb74db1b5a7fbfc470419f3ddf783f6f40642b
Deathreus <csmoore96@yahoo.com>
2019-02-15 22:52:03 -0500

    Add `IsDistToBetween`
    Returns if the distance between this instance and another vector is between the min and max
    
    Basic helper function for cleanliness

--- gamemodes/zombiesurvival/gamemode/obj_vector_extend.lua
+++ gamemodes/zombiesurvival/gamemode/obj_vector_extend.lua
@@ -4,3 +4,8 @@ if not meta then return end
 function meta:DistanceZSkew(vec, skew)
 	return math.sqrt((self.x - vec.x) ^ 2 + (self.y - vec.y) ^ 2 + ((self.z - vec.z) * skew) ^ 2)
 end
+
+function meta:IsDistToBetween(other, min, max)
+	local distance = self:Distance(other)
+	return distance >= min and distance <= max
+end
